2115c2e90196a264986b60ff39ac5e98b75876f1
Sarah Fogalin <sarahfogalin@gmail.com>
2025-03-22 21:57:54 -0400

    Refactor FiltersCard.js

--- src/FiltersCard/FiltersCard.js
+++ src/FiltersCard/FiltersCard.js
@@ -3,32 +3,28 @@ import React from "react";
 // styles
 import "./FiltersCard.css";
 
+const SelectedFilter = ({ label, onRemove }) => (
+  <div className="selected-filter">
+    {label}
+    <span className="delete-button" onClick={onRemove}>
+      <img src="/images/icon-remove.svg" alt="delete" className="delete-icon" />
+    </span>
+  </div>
+);
+
 const FiltersCard = ({ filters, removeFilter, clearFilters }) => {
   return (
     <div className="filter-card card">
       <div className="flex-wrap">
-        {filters &&
-          filters.map((filter) => {
-            return (
-              <div key={filter}>
-                <div className="selected-filter">
-                  {filter}
-                  <span
-                    className="delete-button"
-                    onClick={() => removeFilter(filter)}
-                  >
-                    <img
-                      src="/images/icon-remove.svg"
-                      alt="delete"
-                      className="delete-icon"
-                    />
-                  </span>
-                </div>
-              </div>
-            );
-          })}
+        {filters?.map((filter) => (
+          <SelectedFilter
+            key={filter}
+            label={filter}
+            onRemove={() => removeFilter(filter)}
+          />
+        ))}
       </div>
-      <div className="clear-button" onClick={() => clearFilters()}>
+      <div className="clear-button" onClick={clearFilters}>
         Clear
       </div>
     </div>
